1c0025ab2c619f2bdbe9b41eb0c3bdfb8d09926a
MiladinKi <miladinkovacevic12@gmail.com>
2025-01-28 10:48:20 +0100

    controller for earnings by tickets

--- src/main/java/gym_tickets/repositories/TicketRepository.java
+++ src/main/java/gym_tickets/repositories/TicketRepository.java
@@ -1,7 +1,16 @@
 package gym_tickets.repositories;
 
 import gym_tickets.entities.TicketEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TicketRepository extends CrudRepository<TicketEntity, Integer> {
+
+
+    public List<TicketEntity> findAllByDateOfIssueBetween(@Param("from") LocalDate from, @Param("to") LocalDate to);
 }
